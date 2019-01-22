2bbc2ae488aec124643a4482c2004936ba96c487
Martín Figueroa Padilla <martnfigueroa21@gmail.com>
2019-01-22 15:12:52 -0600

    Login + Registro init

--- Tarea-1/scripts/app.js
+++ Tarea-1/scripts/app.js
@@ -1,4 +1,8 @@
 import {MDCRipple} from '@material/ripple/index';
+import {MDCTextField} from '@material/textfield';
+
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+const passwordField = new MDCTextField(document.querySelector('.mdc-password-field'));
 
 console.log('hello world');
