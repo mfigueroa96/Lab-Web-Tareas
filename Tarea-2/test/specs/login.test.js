8b07b89fe2f0b75edcaf160a803f010227d526c8
Martín Figueroa Padilla <martnfigueroa21@gmail.com>
2019-02-06 18:55:31 -0600

    Test Login

---
+++ Tarea-2/test/specs/login.test.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+
+describe('Login', () => {
+    before(() => {
+        console.log('Signup test running...');
+    });
+
+    it('Successfull Login', () => {
+        browser.url('/');
+        var username = $('#username-field');
+        username.setValue('user01');
+        var password = $('#password-field');
+        password.setValue('class');
+        var button = $('#login-btn');
+        button.click();
+        var target = browser.getTitle();
+        assert.equal(target, 'Welcome!');
+    });
+
+    after(() => {
+        console.log('Signup test ending...');
+    });
+});
---
+++ Tarea-2/test/specs/signup.test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+
+describe('Login', () => {
+    before(() => {
+        console.log('Signup test running...');
+    });
+    
+    it('Successfull Login', () => {
+        browser.url('/');
+        var _;
+    });
+
+    after(() => {
+
+    });
+});
