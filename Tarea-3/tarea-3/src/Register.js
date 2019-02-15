7f610a371caedecccfc335c712f742522719a43f
Patricia Dávila <pattyds.1796@gmail.com>
2019-02-14 22:23:28 -0600

    domicilio

--- Tarea-3/tarea-3/src/Register.js
+++ Tarea-3/tarea-3/src/Register.js
@@ -4,18 +4,39 @@ import Headline from './Headline';
 import Section from './Section';
 import LineLayout from './LineLayout';
 import Input from './Input';
+import SimpleSelect from './SimpleSelect';
+import { FormControl } from '@material-ui/core';
 
+  
 const Register = () => {
     return (
         <div>
             <Headline text="Registráte" />
-            <Section title="Datos personales">
-                <LineLayout>
-                    <Input id="name-field" label="Nombre" />
-                    <Input id="" label="Apellidos" />
-                    <Input id="" label="Correo" />
-                </LineLayout>
-            </Section>
+            <FormControl variant="filled">
+                <Section title="Datos personales">
+                    <LineLayout>
+                        <Input id="name-field" label="Nombre" />
+                        <Input id="" label="Apellidos" />
+                        <Input id="" label="Correo" />
+                    </LineLayout>
+                </Section>
+                <Section title="Domicilio">
+                    <LineLayout>
+                        <Input id="address-field" label="Dirección" />
+                    </LineLayout>
+                    <LineLayout>
+                        <Input id="colony-field" label="Barrio o Colonia" />
+                        <Input id="cp-field" label="C.P." />
+                    </LineLayout>
+                    <LineLayout>
+                        <Input id="city-field" label="Ciudad" />
+                        <Input id="state-field" label="Estado" />
+                    </LineLayout>
+                    <LineLayout>
+                        <SimpleSelect></SimpleSelect>
+                    </LineLayout>
+                </Section>
+            </FormControl>
         </div>
     );
 };
---
+++ Tarea-3/tarea-3/src/SimpleSelect.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FilledInput from '@material-ui/core/FilledInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+
+
+class SimpleSelect extends React.Component {
+  state = {
+    country: '',
+    labelWidth: 10,
+  };
+
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+        <FormControl variant="outlined">
+          <InputLabel ref={ref => {
+              this.InputLabelRef = ref;
+            }} htmlFor="country-field">Pais</InputLabel>
+          <Select
+            value={this.state.country}
+            onChange={this.handleChange}
+            input={<OutlinedInput labelWidth={this.state.labelWidth} name="country" id="country-field" />}
+          >
+            <MenuItem value={"argentina"}>Argentina</MenuItem>
+            <MenuItem value={"chile"}>Chile</MenuItem>
+            <MenuItem value={"colombia"}>Colombia</MenuItem>
+            <MenuItem value={"ecuador"}>Ecuador</MenuItem>
+            <MenuItem value={"mexico"}>México</MenuItem>
+            <MenuItem value={"peru"}>Perú</MenuItem>
+          </Select>
+        </FormControl>
+    );
+  }
+}
+
+SimpleSelect.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default (SimpleSelect);
