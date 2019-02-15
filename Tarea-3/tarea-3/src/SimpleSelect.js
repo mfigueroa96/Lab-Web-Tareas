37ea4d232e5b1e5b67bbd749a010d68c062b09ca
Patricia Dávila <pattyds.1796@gmail.com>
2019-02-14 22:55:51 -0600

    Update SimpleSelect.js

--- Tarea-3/tarea-3/src/SimpleSelect.js
+++ Tarea-3/tarea-3/src/SimpleSelect.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
-import FilledInput from '@material-ui/core/FilledInput';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
@@ -25,8 +20,6 @@ class SimpleSelect extends React.Component {
   };
 
   render() {
-    const { classes } = this.props;
-
     return (
         <FormControl variant="outlined">
           <InputLabel ref={ref => {
