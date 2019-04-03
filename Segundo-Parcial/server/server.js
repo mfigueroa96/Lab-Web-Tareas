116776257df69ed9adb97a1568a941eb699ad6cc
Charlhye <carlosbmtz@gmail.com>
2019-04-02 21:42:26 -0600

    Control desconectado

--- Segundo-Parcial/server/server.js
+++ Segundo-Parcial/server/server.js
@@ -29,6 +29,37 @@ app.get('/api/tequila/:key', (req, res) => {
     });
 
     res.send({ status: 200, my_tequila, key });
-})
+});
+
+app.get('/api/user/:username', (req, res)=>{
+    var username = req.params.username;
+
+    var found = false;
+    var history = [];
+    var my_tequila = null;
+
+    USERS.find(user => user.username == username).
+    tequilas.some(tequisquiapan => {
+        PROVIDERS.some(provider => {
+            provider.tequilas.some(tequila => {
+                if (tequila.serial_numbers.includes(tequisquiapan)) {
+                    my_tequila = Object.assign({}, tequila);
+                    delete my_tequila.serial_numbers;
+                    found = true;
+                    return true;
+                }
+            });
+            if (found) {
+                my_tequila.brand = provider.brand;
+                history.push(my_tequila);
+                my_tequila = null;
+                found = false;
+                return true;
+            }
+        });
+    });
+
+    res.send({ status:200, history, username });
+});
 
 app.listen(8080, (req, res) => console.log('Running'));
