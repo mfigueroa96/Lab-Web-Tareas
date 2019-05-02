6fe7f0ae3385d9cc6a28ebc4cc3af700adf2b675
Martín Figueroa Padilla <martnfigueroa21@gmail.com>
2019-05-02 00:36:19 -0500

    Init

---
+++ Final/services/config.js
@@ -0,0 +1,12 @@
+const config = {
+    ports: {
+        providersAPI: 5001,
+        providersGetInfo: 5002,
+        tequilasAPI: 5003,
+        tequilasGetInfo: 5004,
+        usersAPI: 5005,
+        usersGetInfo: 5006
+    }
+}
+
+module.exports = config;
